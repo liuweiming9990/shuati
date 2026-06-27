a187ae58ea6975df5cfb5514cd098db4dd0f648b
liuweiming9990 <893156555@qq.com>
2026-06-27 08:21:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,48 +1,29 @@
-const CACHE_NAME = 'shuati-v10';
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(['./', './index.html']);
+// 网络优先策略，自动更新
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).then(response => {
+      const cloned = response.clone();
+      caches.open('shuati-dynamic').then(cache => cache.put(event.request, cloned));
+      return response;
+    }).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
 
+// 安装时跳过等待
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+// 激活时立即接管所有页面
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.url.includes('index.html') || event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).then(response => {
-        const cloned = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
-        return response;
-      }).catch(() => caches.match(event.request))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(cached => {
-        return cached || fetch(event.request).then(response => {
-          const cloned = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, cloned));
-          return response;
-        });
-      })
-    );
-  }
-});
-
+// 接收来自页面的“检查更新”消息
 self.addEventListener('message', event => {
-  if (event.data === 'skip-waiting') {
+  if (event.data === 'check-update') {
     self.skipWaiting();
     self.clients.matchAll().then(clients => {
       clients.forEach(client => client.postMessage('reload'));
